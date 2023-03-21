bfab44e26b9553d73649dc1d7f407d359f5bde38
Florida <florida.elago@gmail.com>
2023-03-21 16:04:12 -0400

    update to use typescript and add singlylinkedlist implementation

---
+++ src/SinglyLinkedList.ts
@@ -0,0 +1,10 @@
+class LinkedListNode<T> {
+  value: T;
+  next: LinkedListNode<T> | null;
+
+  /**
+   * Create a new linked list node.
+   * @param {T} value - The value of the node.
+   * @param { LinkedListNode<T> | null} next - the next node in the linked list
+   */
+}
